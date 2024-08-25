11d36837e37c169869b4af8d18ef682f3d434f9e
Konstantin <Kostick_zenin@mai.ru>
2024-08-25 16:32:52 +0300

    HW11 - добавил аннотацию типов

--- work11_main.py
+++ work11_main.py
@@ -1,8 +1,14 @@
 import json
-
-from Data.cities import cities_list
 from pprint import pprint
 from random import randint
+
+try:
+    from Data.cities import cities_list
+except ModuleNotFoundError:
+    print("Модуль cities не найден, пожалуйста импортируйте его в папку \"Data\" "
+          "или укажите корректный путь до него в строке №6")
+    exit()
+
 
 rules = (
     "Правила игры «Города»: Каждый участник называет реально существующий в данный момент времени город"
@@ -16,7 +22,7 @@ rules = (
     "однако из-за продолжительности и монотонности игрового процесса игра может закончиться ничьей"
     )
 
-json_file = "./data/cities.json"
+json_file_path = "./Data/cities.json"
 
 player_input = ""
 
@@ -25,24 +31,40 @@ player_score = 0
 computer_score = 0
 
 
-# cities_set = {city["name"].lower() for city in cities_list}
-# cities_set = {"яхрома", "абаза", "абдулино", "орёл", "лабинск", "калининград", "вад"}
+def writing_cities(cities_list: list):
+    """
+    Записывает данные из cities_list в файл json
+    :param cities_list: список, импортируемый из файла cities.py
+    """
+    with open(json_file_path, "w", encoding="utf-8") as file:
+        json.dump(cities_list, file, ensure_ascii=False, indent=4)
 
-with open(json_file, "w", encoding="utf-8") as file:
-    json.dump(cities_list, file, ensure_ascii=False, indent=4)
 
-with open(json_file, "r", encoding="utf-8") as file:
-    cities_list_from_fson = json.load(file)
+def read_cities() -> set:
+    """
+    Импортирует данные из файла json
+    :return: сет городов
+    """
+    try:
+        with open(json_file_path, "r", encoding="utf-8") as file:
+            cities_list_from_json = json.load(file)
+            cities_set = {city["name"].lower() for city in cities_list_from_json}
+            return cities_set
+    except FileNotFoundError:
+        writing_cities(cities_list)
+        with open(json_file_path, "r", encoding="utf-8") as file:
+            cities_list_from_json = json.load(file)
+            cities_set = {city["name"].lower() for city in cities_list_from_json}
+            return cities_set
 
-cities_set = {city["name"].lower() for city in cities_list_from_fson}
 
-print(cities_set)
+cities_set: set = read_cities()
 
-player_cities = set()
+player_cities: set = set()
 
-computer_cities = set()
+computer_cities: set = set()
 
-bad_letters = set()
+bad_letters: set = set()
 
 turn = 200
 
@@ -58,7 +80,11 @@ for city in cities_set:
 last_letter_1 = ""
 
 
-def last_letter_determining(city):
+def determining_last_letter(city: str):
+    """
+    Функция определяет последнюю букву в названом городе
+    :param city: Названый город
+    """
     global last_letter_1
     indent = -1
     if city[-1] in bad_letters:
@@ -68,12 +94,20 @@ def last_letter_determining(city):
         last_letter_1 = city[indent]
 
 
-def first_turn_determining():
+def determining_first_turn() -> int:
+    """
+    Функция генерирует случайное число для определения первого хода
+    :return: случайное число от 0 до 10
+    """
     first_turn = randint(0, 10)
     return first_turn
 
 
-def find_cities_help():
+def find_cities_help() -> set:
+    """
+    Функция формирует подсказку для хода игрока
+    :return: сет с городами, которые можно назвать.
+    """
     global last_letter_1
     alphabet = {
         "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о",
@@ -86,7 +120,11 @@ def find_cities_help():
     return cities_help_set
 
 
-def player_turn():
+def player_turn() -> tuple:
+    """
+    Ход игрока
+    :return: кортеж с данными хода игрока
+    """
     name = "игрок"
     help_1 = find_cities_help()
     pprint(help_1)
@@ -96,7 +134,11 @@ def player_turn():
     return city, name, turn
 
 
-def computer_turn():
+def computer_turn() -> tuple:
+    """
+    Ход компьютера
+    :return: кортеж с данными хода компьютера
+    """
     global city
     name = "компьютер"
     computer_turn_set = find_cities_help()
@@ -109,9 +151,12 @@ def computer_turn():
 
 
 def game():
+    """
+    Базовая версия игры
+    """
     global turn
     if turn == 200:
-        turn = first_turn_determining()
+        turn = determining_first_turn()
 
     while cities_set:
         if turn < 5:
@@ -125,7 +170,7 @@ def game():
                 pprint(f"Поздравляю! вы победили в игре в города назвав следующие города: {player_cities}"
                        f"а компьютер: {computer_cities}")
                 exit()
-            last_letter_determining(city)
+            determining_last_letter(city)
             computer_cities.add(city)
             print(f"Компьютер назвал город {city}, значит вам нужно назвать город,"
                   f"начинающийся на букву {last_letter_1}")
@@ -144,7 +189,7 @@ def game():
                 pprint(f"К сожалению вы ввели неправильный или несуществующий город и проиграли!"
                        f"Вы смогли назвать следующие города: {player_cities}, а компьютер: {computer_cities}")
                 exit()
-            last_letter_determining(city)
+            determining_last_letter(city)
             player_cities.add(city)
             print(f"Вы назвали город {city}")
 
@@ -152,7 +197,6 @@ def game():
 print(f"Давай сыграем в игру города! Правила у неё довольно простые:")
 pprint(rules)
 print("Будь внимателен названия городов нужно вводить точно и без знаков препинания и пробелов")
-game()
 
 game()
 
